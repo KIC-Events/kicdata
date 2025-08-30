a6c7dde051f5d814ce146e2bed5509472d291cd2
jarebearzilla <spiritualpenguin71@gmail.com>
2025-08-29 23:44:38 -0400

    Updating BillingContact class

--- Models/WebModels/PaymentModels/BillingContact.cs
+++ Models/WebModels/PaymentModels/BillingContact.cs
@@ -1,4 +1,4 @@
-
+using System.ComponentModel.DataAnnotations;
 
 namespace KiCData.Models.WebModels.PaymentModels
 {
@@ -6,14 +6,37 @@ namespace KiCData.Models.WebModels.PaymentModels
     
     public class BillingContact
     {
+
+        [Required]
+        [Display(Name = "Address Line 1")]
+        public string AddressLine1 { get; set; }
+
+        [Display(Name = "Address Line 2")]
+        public string AddressLine2 { get; set; }
+
         public string[] AddressLines { get; set; }
-        public string FamilyName{ get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string FamilyName { get; set; }
+
+        [Required]
+        [Display(Name = "First Name")]
         public string GivenName { get; set; }
+
+        [Required]
+        [Display(Name = "Email")]
+        [EmailAddress]
         public string EmailAddress { get; set; }
         public string CountryCode { get; set; }
+
+        [Required]
+        [Display(Name = "Phone Number")]
         public string PhoneNumber { get; set; }
         public string State { get; set; }
         public string City { get; set; }
+
+        [Display(Name = "Zip")]
         public string PostalCode { get; set; }
         
         public BillingContact(string[] addressLines, string familyName, string givenName, string emailAddress, string countryCode, string phoneNumber, string state, string city, string postalCode)
